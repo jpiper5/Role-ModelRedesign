85e4baa8cb9eca059da55aedb3de7a10f1de9646
jpiper5 <jpiper5@asu.edu>
2024-10-09 22:13:30 -0700

    added files

---
+++ script.js
@@ -0,0 +1,11 @@
+// global use strict
+"use strict";
+
+//light/dark mode
+function darkMode(){
+    let text = document.querySelector("#intro")
+    
+}
+
+document.getElementById("darkMode").addEventListener("click", darkMode);
+<script src="script.js" async></script>
